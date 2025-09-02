05781676fb4541e7550bfdf1dd19735bda134ee9
Juan Andres Veru Sarmiento <juanandresveru@gmail.com>
2025-09-02 17:48:50 -0500

    se adapto el proyecto para conectar los microservicios

--- users/api/user_viewset.py
+++ users/api/user_viewset.py
@@ -1,6 +1,16 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from users.models.user import User
+from users.serializers.user_serializes.user_serializer import UserSerializer
 
 class UserViewSet(viewsets.ViewSet):
-    #métodos de la vista para users
-    pass
+
+    def create(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"message": "Usuario creado exitosamente"},
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
