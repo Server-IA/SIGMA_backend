d15d3c3a10d758c976cb989c0ebc64ff603fb97c
Juan Andres Veru Sarmiento <85792843+JuanVeru@users.noreply.github.com>
2025-09-03 23:31:07 -0500

    Update urls.py

--- machpaymanager/urls.py
+++ machpaymanager/urls.py
@@ -3,12 +3,12 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', include('machinery.urls')),
-    path('main/', include('maintenance.urls')),
-    path('main/', include('monitoring.urls')),
-    path('main/', include('parameterization.urls')),
-    path('main/', include('payroll.urls')),
-    path('main/', include('service_requests.urls')),
-    path('main/', include('users.urls')),
+    path('', include('machinery.urls')),
+    path('', include('maintenance.urls')),
+    path('', include('monitoring.urls')),
+    path('', include('parameterization.urls')),
+    path('', include('payroll.urls')),
+    path('', include('service_requests.urls')),
+    path('', include('users.urls')),
 
 ]
