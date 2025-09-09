84f918c1709cf7d9f61e02fa5c57a9f08acc2369
jbeleno <u20212201817@usco.edu.co>
2025-09-09 11:45:43 -0500

    se agregaron las modificaciones de la historia de usuario y asi mismo nuevas validaciones de colores y contrastes

--- parameterization/serializers/visual_parameterization_serializers/visual_parameterization_list_serializer.py
+++ parameterization/serializers/visual_parameterization_serializers/visual_parameterization_list_serializer.py
@@ -10,12 +10,23 @@ class VisualParameterizationListSerializer(serializers.ModelSerializer):
             'id_visual_parameterization',
             'name',
             'description',
+            # Colores del sistema de diseño
+            'primary_color',
+            'secondary_color',
+            'accent_color',
             'background_color',
+            'surface_color',
             'text_color',
-            'font',
-            'font_size',
-            'border_thickness',
+            'text_secondary_color',
             'border_color',
+            'hover_color',
+            'error_color',
+            'success_color',
+            'warning_color',
+            # Tipografía
+            'font',
+            'title_size',
+            'paragraph_size',
             'visual_parameterization_status',
             'visual_parameterization_status_name'
         ]
