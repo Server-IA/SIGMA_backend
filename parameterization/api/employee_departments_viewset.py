3525fa1d0ca6913755a6699e11d0fb8b2787682d
Juan Andres Veru Sarmiento <juanandresveru@gmail.com>
2025-09-09 22:38:36 -0500

    se añadieron endpoints de la gestion de departamentos

--- parameterization/api/employee_departments_viewset.py
+++ parameterization/api/employee_departments_viewset.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import action
 from parameterization.models import EmployeeDepartment, Statues
 from parameterization.serializers.employee_departments_serializers.employee_departments_create_serializer import EmployeeDepartmentCreateSerializer
 from parameterization.serializers.employee_departments_serializers.employee_departments_list_serializer import EmployeeDepartmentListSerializer
-
+from django.shortcuts import get_object_or_404
 
 class EmployeeDepartmentViewSet(viewsets.ViewSet):
 
@@ -34,3 +34,37 @@ class EmployeeDepartmentViewSet(viewsets.ViewSet):
         departments = EmployeeDepartment.objects.all()
         serializer = EmployeeDepartmentListSerializer(departments, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=['get'], url_path='list/active')
+    def listar_activos(self, request):
+        departments = EmployeeDepartment.objects.filter(id_statues_id=1)
+        if not departments.exists():
+            return Response({
+                "message": "No existen departamentos activos registrados",
+                "data": []
+            }, status=status.HTTP_200_OK)
+        serializer = EmployeeDepartmentListSerializer(departments, many=True)
+        return Response({
+            "message": "Departamentos activos obtenidos exitosamente",
+            "data": serializer.data
+        }, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['patch'], url_path='toggle-status')
+    def toggle_status(self, request, pk=None):
+        department = get_object_or_404(EmployeeDepartment, pk=pk)
+
+        if department.id_statues_id == 1:
+            try:
+                department.id_statues = Statues.objects.get(pk=2)
+            except Statues.DoesNotExist:
+                return Response({"error": "El estado con id=2 no existe"}, status=status.HTTP_400_BAD_REQUEST)
+            message = "Departamento desactivado exitosamente"
+        else:
+            try:
+                department.id_statues = Statues.objects.get(pk=1)
+            except Statues.DoesNotExist:
+                return Response({"error": "El estado con id=1 no existe"}, status=status.HTTP_400_BAD_REQUEST)
+            message = "Departamento activado exitosamente"
+
+        department.save(update_fields=['id_statues'])
+        return Response({"message": message}, status=status.HTTP_200_OK)
--- parameterization/serializers/employee_departments_serializers/employee_departments_list_serializer.py
+++ parameterization/serializers/employee_departments_serializers/employee_departments_list_serializer.py
@@ -2,10 +2,12 @@ from rest_framework import serializers
 from parameterization.models import EmployeeDepartment
 
 class EmployeeDepartmentListSerializer(serializers.ModelSerializer):
+    estado = serializers.CharField(source='id_statues.name', read_only=True)
     class Meta:
         model = EmployeeDepartment
         fields = [
             'id_employee_department',
             'name',
             'description',
+            'estado'
         ]
