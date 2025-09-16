dcaef0c2e2af28eb26349d0192c53ecbc7135a6b
Juan Andres Veru Sarmiento <juanandresveru@gmail.com>
2025-09-16 13:47:49 -0500

    se actualizo el init.sql

--- init.sql
+++ init.sql
@@ -13,27 +13,27 @@ GRANT ALL PRIVILEGES ON DATABASE auditdb TO postgres;
 
 -- Crear tabla de auditoría
 CREATE TABLE IF NOT EXISTS audit_events (
-    event_id    uuid PRIMARY KEY,
-    ts          timestamptz NOT NULL DEFAULT now(),
-    actor_id    text,
-    actor_role  text,
-    actor_type  text,          -- user | service
-    request_id  text,
-    ip          inet,
-    user_agent  text,
-    service     text NOT NULL, -- users | machinery | payroll | ...
-    module      text NOT NULL, -- gestion_usuarios | nomina | ...
-    submodule   text,          -- NUEVO: roles, permisos, etc.
-    feature     text,          -- NUEVO: create_role, edit_role, etc.
-    object_type text,
-    object_id   text,
-    operation   text NOT NULL, -- ACCESS | REGISTER | UPDATE | ...
-    before      jsonb,
-    after       jsonb,
-    meta        jsonb
+    event_id      uuid PRIMARY KEY,
+    ts            timestamptz NOT NULL DEFAULT now(),
+    actor_id      text,
+    actor_role    text,
+    request_id    text,
+    ip            inet,
+    user_agent    text,
+    module        text NOT NULL,  -- p.ej. gestion_usuarios
+    submodule     text,           -- p.ej. roles, permisos
+    feature       text,           -- p.ej. edit, create
+    object_type   text,
+    object_id     text,
+    operation     text NOT NULL,  -- ACCESS | REGISTER | UPDATE | ...
+    before        jsonb,
+    after         jsonb,
+    meta          jsonb,
+    permission_id integer,        -- ID del permiso bajo el cual se ejecutó la acción
+    diff          jsonb           -- diff calculado entre before/after (estructura la definimos en app)
 );
 
--- Índices
+-- Índices útiles
 CREATE INDEX IF NOT EXISTS idx_audit_ts
     ON audit_events (ts);
 
@@ -52,7 +52,12 @@ CREATE INDEX IF NOT EXISTS idx_audit_submodule
 CREATE INDEX IF NOT EXISTS idx_audit_feature
     ON audit_events (feature);
 
--- (Opcional) índice sobre campo JSON
--- CREATE INDEX IF NOT EXISTS idx_audit_meta_source
---     ON audit_events ((meta->>'source'));
+CREATE INDEX IF NOT EXISTS idx_audit_permission_id
+    ON audit_events (permission_id);
+
+CREATE INDEX IF NOT EXISTS idx_audit_meta_source
+    ON audit_events ((meta->>'source'));
+
+CREATE INDEX IF NOT EXISTS idx_audit_meta_result
+    ON audit_events ((meta->>'result'));
 
