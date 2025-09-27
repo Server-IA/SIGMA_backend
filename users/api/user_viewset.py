0db6c80ba80425cc4fc46db724f631e1650c25ad
Juan Andres Veru Sarmiento <juanandresveru@gmail.com>
2025-09-26 21:02:39 -0500

    resolucion permiso visual

--- users/api/user_viewset.py
+++ users/api/user_viewset.py
@@ -1,9 +1,11 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, permissions
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from users.models.user import User
 from users.serializers.user_serializes.user_serializer import UserSerializer
 
 class UserViewSet(viewsets.ViewSet):
+    permission_classes = [permissions.AllowAny]  # Allow unauthenticated access by default
 
     def create(self, request):
         serializer = UserSerializer(data=request.data)
