3a76b10e8352216432ec5218f4ac5114a27e6761
Juan Andres Veru Sarmiento <juanandresveru@gmail.com>
2025-10-08 21:51:08 -0500

    se implemento el endpoint de listar tipos de personas

--- service_requests/urls.py
+++ service_requests/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 from service_requests.api.customer_viewset import CustomerViewSet
+from service_requests.api.person_type_viewset import PersonTypeViewSet
 
 router = routers.DefaultRouter()
 
 router.register(r'customers', CustomerViewSet, basename='customer')
+router.register(r'person_types', PersonTypeViewSet, basename='person_type')
 
 urlpatterns = [
     path('', include(router.urls))
