ecce1414f909a8147c5bb151538a2b92318820ed
Juan Andres Veru Sarmiento <juanandresveru@gmail.com>
2025-10-21 05:58:08 -0500

    Implementacion registrar dispositivo y listar parametros

--- machinery/models/telemetry_devices.py
+++ machinery/models/telemetry_devices.py
@@ -3,6 +3,7 @@ from django.db import models
 class TelemetryDevices(models.Model):
     id_device = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50, null=False, blank=False)
+    IMEI = models.BigIntegerField(null=False, blank=False)
     id_statues = models.ForeignKey('parameterization.Statues', on_delete=models.PROTECT)
     registration_date = models.DateTimeField(auto_now=True)
     modification_date = models.DateTimeField(auto_now=True)
