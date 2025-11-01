f6c677caa53d70f9304d6b1a9a17ba7ed16a96db
Juan David Lara León <87272795+JuanDLara@users.noreply.github.com>
2025-10-31 21:03:42 -0500

    Import sys module in settings.py

--- machpaymanager/settings.py
+++ machpaymanager/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
