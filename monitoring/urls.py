7165cd4a813696ac4507f9d8283aed341765e587
Juan Andres Veru Sarmiento <85792843+JuanVeru@users.noreply.github.com>
2025-11-03 23:49:18 -0500

    Revert "Revert "feat: Integración de alertas en telemetría y prevención de procesamie…""

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
+from monitoring.views.health import health_check
 
 router = routers.DefaultRouter()
 
 urlpatterns = [
+    path('health/', health_check, name='health_check'),
     path('', include(router.urls))
 ]
