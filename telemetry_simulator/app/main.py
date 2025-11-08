066b36d010ff2f481750f2f1188ebf6d639cb934
nathaliasal <91170630+nathaliasal@users.noreply.github.com>
2025-11-08 18:46:20 -0500

    Change Uvicorn port from 8000 to 8010

--- telemetry_simulator/app/main.py
+++ telemetry_simulator/app/main.py
@@ -216,5 +216,5 @@ async def shutdown_event():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8010, reload=True)
 
