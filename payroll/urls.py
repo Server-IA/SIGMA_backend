9f05424833dc5d309470bddb321430395352d36a
nathaliasal <91170630+nathaliasal@users.noreply.github.com>
2025-11-26 10:55:38 -0500

    Add PayrollHistoryReportViewSet to URLs

--- payroll/urls.py
+++ payroll/urls.py
@@ -6,6 +6,7 @@ from .api.employee_viewset import EmployeeViewSet
 from .api.temporary_payroll_adjustment_viewset import TemporaryPayrollAdjustmentViewSet
 from .api.employee_news_viewset import EmployeeNewsViewSet
 from .api.payroll_viewset import PayrollViewSet
+from .api.payroll_history_report_viewset import PayrollHistoryReportViewSet
 
 router = routers.DefaultRouter()
 router.register(r'established_contracts', EstablishedContractViewSet, basename='established-contract')
@@ -14,6 +15,7 @@ router.register(r'employees', EmployeeViewSet, basename='employee')
 router.register(r'temporary_adjustments', TemporaryPayrollAdjustmentViewSet, basename='temporary_adjustment')
 router.register(r'employee_news', EmployeeNewsViewSet, basename='employee-news')
 router.register(r'payroll', PayrollViewSet, basename='payroll')
+router.register(r'payroll_history_reports', PayrollHistoryReportViewSet, basename='payroll-history-report')
 
 urlpatterns = [
     path('', include(router.urls))
