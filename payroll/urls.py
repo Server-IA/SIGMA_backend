e4a4197e1a43b095ef27bb69c69f4a508be58ac9
Krlitoxprz <u201821721397@usco.edu.co>
2025-11-26 01:04:17 -0500

    Implementación de reporte de historial de nómina

--- payroll/urls.py
+++ payroll/urls.py
@@ -5,6 +5,7 @@ from .api.days_of_week_viewset import DaysOfWeekViewSet
 from .api.employee_viewset import EmployeeViewSet
 from .api.temporary_payroll_adjustment_viewset import TemporaryPayrollAdjustmentViewSet
 from .api.employee_news_viewset import EmployeeNewsViewSet
+from .api.payroll_history_report_viewset import PayrollHistoryReportViewSet
 
 router = routers.DefaultRouter()
 router.register(r'established_contracts', EstablishedContractViewSet, basename='established-contract')
@@ -12,6 +13,7 @@ router.register(r'days_of_week', DaysOfWeekViewSet, basename='days_of_week')
 router.register(r'employees', EmployeeViewSet, basename='employee')
 router.register(r'temporary_adjustments', TemporaryPayrollAdjustmentViewSet, basename='temporary_adjustment')
 router.register(r'employee_news', EmployeeNewsViewSet, basename='employee-news')
+router.register(r'payroll_history_reports', PayrollHistoryReportViewSet, basename='payroll-history-report')
 
 urlpatterns = [
     path('', include(router.urls))
