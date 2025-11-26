f6d26ecbe73539721182215759dbf298f1167d11
birdm4nw <davidf29gutierrez@gmail.com>
2025-11-25 22:25:23 -0500

    feat:Implementación de endpoint de descarga de historial de contratos y cargos de empleado

--- payroll/urls.py
+++ payroll/urls.py
@@ -5,6 +5,7 @@ from .api.days_of_week_viewset import DaysOfWeekViewSet
 from .api.employee_viewset import EmployeeViewSet
 from .api.temporary_payroll_adjustment_viewset import TemporaryPayrollAdjustmentViewSet
 from .api.employee_news_viewset import EmployeeNewsViewSet
+from .api.contract_history_report_viewset import ContractHistoryReportViewSet
 
 router = routers.DefaultRouter()
 router.register(r'established_contracts', EstablishedContractViewSet, basename='established-contract')
@@ -12,6 +13,7 @@ router.register(r'days_of_week', DaysOfWeekViewSet, basename='days_of_week')
 router.register(r'employees', EmployeeViewSet, basename='employee')
 router.register(r'temporary_adjustments', TemporaryPayrollAdjustmentViewSet, basename='temporary_adjustment')
 router.register(r'employee_news', EmployeeNewsViewSet, basename='employee-news')
+router.register(r'reports', ContractHistoryReportViewSet, basename='reports')
 
 urlpatterns = [
     path('', include(router.urls))
